f3191da69f25758fcbd7f355cc23b1b9c9e68cc7
Alejandro Gallo <alejandro.gallo@globalprocessing.com.ar>
2022-11-14 21:21:44 -0300

    Agregados de la clase 14/11

--- ProyectoFinal/Sitio/urls.py
+++ ProyectoFinal/Sitio/urls.py
@@ -5,5 +5,6 @@ from Sitio import views
 urlpatterns = [
     path('',views.inicio),
     path('mostrar_persona/',views.mostrar_personas, name='Personas'),
-    
+    path('curso_list', views.CursoList.as_view(),),
+    path('curso_detail', views.CursoDetail.as_view())
 ]
