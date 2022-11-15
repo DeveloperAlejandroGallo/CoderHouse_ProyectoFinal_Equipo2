f4596de7cbdd196b364f7eb3ff70dee2ea45ceb3
Alejandro Gallo <alejandro.gallo@globalprocessing.com.ar>
2022-11-14 21:23:02 -0300

    Agregado de clase 14/11

--- ProyectoFinal/Sitio/models.py
+++ ProyectoFinal/Sitio/models.py
@@ -5,9 +5,12 @@ import datetime
 class Persona(models.Model):
 
     nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
     edad = models.IntegerField()
     fecha_nacimiento = models.DateField()
     email = models.EmailField()
+
+
 
 
 class Curso(models.Model):
@@ -15,9 +18,13 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     codigo = models.IntegerField()
 
+    def __str__(self) -> str:
+        return super().__str__(f'Curso: {self.nombre} - Codigo {self.codigo}')
+
 class Profesor(models.Model): 
 
     nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
     edad = models.IntegerField()
     profesion =  models.CharField(max_length=40)
     email = models.EmailField()
--- ProyectoFinal/Sitio/views.py
+++ ProyectoFinal/Sitio/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Persona
+from .models import *
 from django.db import *
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
 
 
 def mostrar_personas(response):
     personas = Persona.objects.all() #Trae todos los registros de la base como una lista
+
     return render(response,'personas.html',{'personas':personas})
 
 def inicio(response):
@@ -16,3 +19,10 @@ def funcion_con_parametros(response):
     lista = [1,2,3,4]
     return render(response, 'prueba.html',{'lista':lista})
 
+class CursoList(ListView):
+    model = Curso
+    template_name = 'cursos_list.html'
+
+class CursoDetail(DetailView):
+    model = Curso
+    template_name = 'cursos_detalle.html'
